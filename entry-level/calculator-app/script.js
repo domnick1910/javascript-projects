56c9f17604ec2c46d069ec3681ab25e0edac1e2b
Dominik Papanek <PAPANEK.Dominik@ipesoft.com>
2024-06-12 08:40:10 +0200

    Uprava vkladania ciselnych hodnot

--- entry-level/calculator-app/script.js
+++ entry-level/calculator-app/script.js
@@ -10,81 +10,14 @@ const btnMultiply = document.getElementById("btn-multiply");
 const btnClearAll = document.getElementById("clear-all");
 const btnClear = document.getElementById("clear");
 
-const btnNumber9 = document.getElementById("btn-number-9");
-const btnNumber8 = document.getElementById("btn-number-8");
-const btnNumber7 = document.getElementById("btn-number-7");
-const btnNumber6 = document.getElementById("btn-number-6");
-const btnNumber5 = document.getElementById("btn-number-5");
-const btnNumber4 = document.getElementById("btn-number-4");
-const btnNumber3 = document.getElementById("btn-number-3");
-const btnNumber2 = document.getElementById("btn-number-2");
-const btnNumber1 = document.getElementById("btn-number-1");
-const btnNumber0 = document.getElementById("btn-number-0");
+const btnNumbers = document.querySelectorAll(".calculator-numbers");
 
-btnNumber0.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '0'
-    }
-    
-});
-
-btnNumber1.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '1'
-    }
-
-});
-
-btnNumber2.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '2'
-    }
-});
-
-btnNumber3.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '3'
-    }
-});
-
-btnNumber4.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '4'
-    }
-});
-
-btnNumber5.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '5'
-    }
-});
-
-btnNumber6.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '6'
-    }
-});
-
-btnNumber7.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '7'
-    }
-});
-
-btnNumber8.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '8'
-    }
-});
-
-btnNumber9.addEventListener("click", ()=>{
-    if (checkLength(inputField.innerText)) {
-        inputField.innerText += '9'
-    }
-});
-
-btnClear.addEventListener("click", ()=>{
-     inputField.innerText = inputField.innerText.subString(1);
+btnNumbers.forEach(button => {
+    button.addEventListener('click', ()=>{
+        if (checkLength(inputField.innerText)) {
+            inputField.innerText += button.innerText;
+        }
+    })
 });
 
 const checkLength = (args) => {
